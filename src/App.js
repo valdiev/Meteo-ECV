4914ab8792f196772ea15a77b494aa9bdf667e70
valdiev <valdiev@live.fr>
2022-02-22 11:57:19 +0100

    Init sass to react project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/style.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>C'est abérrant</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
