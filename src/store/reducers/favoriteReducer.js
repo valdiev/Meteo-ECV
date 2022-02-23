39b3197b0db070ca7ac34bb1e2a48b0340290252
theoBIGBIZYOU <theo@bigbizyou.agency>
2022-02-23 22:58:52 +0100

    add initialFav

--- src/store/reducers/favoriteReducer.js
+++ src/store/reducers/favoriteReducer.js
@@ -3,12 +3,13 @@ import {createSlice, current} from "@reduxjs/toolkit";
 export const favoriteSlice = createSlice({
     name: 'favorite',
     initialState: {
-        listOfFavorite: [{ville: "Oignies", temp: 10, daily: null}]
+        listOfFavorite: [{ville: "Oignies", temp: 10, daily: null, weather: "02n"},
+            {ville: "Sydney", temp: 24, daily: null, weather: "09d"},
+            {ville: "New York", temp: 12, daily: null, weather: "50d"}]
     },
     reducers:{
         addFavorite: (state,action) => {
             state.listOfFavorite.push(action.payload)
-            console.log(current(state.listOfFavorite));
         }
     }
 })
